aa08984a4dd1d602297964e51c3187ec7711ce6b
fjgallard <fjgallardo.github@gmail.com>
2019-03-21 15:54:55 +0800

    Simpler method of implementing seasons

--- src/app/ui/team-page/team-page.component.ts
+++ src/app/ui/team-page/team-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/core/auth.service';
 import { Observable, combineLatest } from 'rxjs';
 import { SeasonService } from 'src/app/core/season.service';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'team-page',
@@ -11,21 +10,11 @@ import { map } from 'rxjs/operators';
 })
 export class TeamPageComponent {
   seasonId$: Observable<string>;
-  userSeasonCombined$: Observable<any>;
 
   constructor(
     public auth: AuthService,
     public seasonService: SeasonService
     ) {
     this.seasonId$ = this.seasonService.getEnabledSeasonId();
-
-    this.userSeasonCombined$ = combineLatest(this.auth.user$, this.seasonId$).pipe(
-      map(([user, seasonId]) => {
-        return {
-          user: user,
-          seasonId: seasonId
-        };
-      })
-    );
   }
 }
