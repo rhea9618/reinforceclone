875e5de0f6d89885a804c1ae0d06012913de0218
jasminemirabueno <jasmine.mirabueno@infor.com>
2019-08-07 17:00:18 +0800

    Closes #90 Update rank label and range

--- src/app/ui/season-rank.pipe.ts
+++ src/app/ui/season-rank.pipe.ts
@@ -1,23 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({name: 'seasonRank'})
+@Pipe({ name: 'seasonRank' })
 export class SeasonRankPipe implements PipeTransform {
 
   transform(exp: number): string {
     // Might be a good idea to determine a formula instead of putting ranges
     switch (true) {
-        case (exp >= 1 && exp < 20) :
-          return 'Rank 1';
-        case (exp >= 20 && exp < 50):
-          return 'Rank 2';
-        case (exp >= 50 && exp < 90):
-          return 'Rank 3';
-        case (exp >= 90 && exp < 150):
-          return 'Rank 4';
-        case (exp >= 150):
-          return 'Rank 5';
-        default:
-          return 'Unranked';
+      case (exp >= 1 && exp <= 40):
+        return 'Nebula';
+      case (exp > 40 && exp <= 150):
+        return 'Star';
+      case (exp > 150 && exp <= 300):
+        return 'Red Giant';
+      case (exp > 300 && exp <= 500):
+        return 'Supernova';
+      case (exp > 500):
+        return 'Neutron';
+      default:
+        return 'Unranked';
     }
   }
 }
