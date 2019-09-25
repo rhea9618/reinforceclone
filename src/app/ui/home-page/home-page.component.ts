ad603e5c86b0d7ff1d6f43d63451add0c3ae415c
gerarrd <jesusgerardderamos@gmail.com>
2019-09-25 15:16:43 +0800

    closes #110

--- src/app/ui/home-page/home-page.component.ts
+++ src/app/ui/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { PlayerPointsService } from '../player-quest/player-points.service';
 import { AuthService } from 'src/app/core/auth.service';
 import { SeasonService } from 'src/app/core/season.service';
+import { TeamsService } from 'src/app/teams/teams.service';
 
 @Component({
   selector: 'home-page',
@@ -13,9 +14,10 @@ import { SeasonService } from 'src/app/core/season.service';
 export class HomePageComponent implements OnInit {
 
   topUsers$: Observable<PlayerPoints[]>;
-  readonly displayedColumns = ['index', 'name', 'points', 'rank', 'info'];
+  readonly displayedColumns = ['index', 'name', 'team', 'points', 'rank', 'info'];
 
   constructor(
+    private team: TeamsService,
     private auth: AuthService,
     private playerPoints: PlayerPointsService,
     private season: SeasonService
